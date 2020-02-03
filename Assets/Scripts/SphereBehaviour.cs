f585006ffdfc63bc799bc2a42a4a06a77e7a11ba
Nícolas Mocaiber Cardoso Cury <nicolas.cury@MJV.net>
2020-02-03 19:17:03 -0300

    slider UI to simulate and test attention input

--- Assets/Scripts/SphereBehaviour.cs
+++ Assets/Scripts/SphereBehaviour.cs
@@ -10,18 +10,22 @@ public class SphereBehaviour : MonoBehaviour {
 	private int attention1;
 	private int signal1;
 
+	public float brainItensity = 1f;
+	public float gravityForce = 9.81f;
+
+	private float slider;
 
 	void Jump ()
 	{
 		
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			rb.AddForce(Vector3.up * 10,ForceMode.VelocityChange);
+			rb.AddForce(Vector3.up * brainItensity,ForceMode.VelocityChange);
 		}
 
 		if (Input.GetKey (KeyCode.Space)) {
 			//if (jumpValue < maxJumpValue) {
-				rb.AddForce(Vector3.up * 10,ForceMode.Acceleration);
+			rb.AddForce(Vector3.up * brainItensity,ForceMode.Acceleration);
 			//}
 
 		}
@@ -50,14 +54,27 @@ public class SphereBehaviour : MonoBehaviour {
 
 	// Update is called once per frame
 
+	public void slider_change(float value)
+	{
+		//transform.position = new Vector3 (transform.position.x, value, transform.position.z);
+		slider = value;
+	}
+
 	void Update () {
 
 
+//		Physics.gravity = new Vector3 (0f, -gravityForce, 0f);
+//
+//		if(transform.position.y <= 9)
+//			rb.AddForce(Vector3.up * (attention1/100f) * brainItensity, ForceMode.Acceleration);
 
-		//Debug.Log ("teste");
-		if(attention1 > 50 && this.transform.position.y < 9)
+
+		if (slider > 50 && transform.position.y <= 9)
 			rb.AddForce(Vector3.up * 9,ForceMode.Acceleration);
-		
+
+		//if (this.transform.position.y >= 9)
+			//rb.AddForce(Vector3.down * 3,ForceMode.Acceleration);
+			
 		Jump ();
 	}
 }
